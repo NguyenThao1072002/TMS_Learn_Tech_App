c707fc5f251d98534d30ca70d56e7b089021891c
Nolan1309 <tranfc911@gmail.com>
2025-06-18 21:42:30 +0700

    up

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,4 +1,4 @@
 class Constants {
-  static const String BASE_URL = 'http://192.168.1.156:8080'; // API URL
-  static const String WS_URL = 'ws://192.168.1.156:8080/ws'; // WebSocket URL
+  static const String BASE_URL = 'http://103.166.143.198:8080'; // API URL
+  static const String WS_URL = 'ws://103.166.143.198:8080/ws'; // WebSocket URL
 }
